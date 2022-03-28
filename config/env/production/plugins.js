ad07d0ccb5c2519ed397344b10f44edc169eac60
Melinda Golden <info@melindagolden.com>
2022-03-27 22:18:59 -0400

    Increase depth limit

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -4,7 +4,7 @@ module.exports = {
       endpoint: '/graphql',
       shadowCRUD: true,
       playgroundAlways: true,
-      depthLimit: 7,
+      depthLimit: 10,
       amountLimit: 100,
       apolloServer: {
         tracing: false,
